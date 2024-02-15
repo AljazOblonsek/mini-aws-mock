e727bf1e3bcb3e049d917043a886b386fd66235f
AljazOblonsek <aljaz.oblonsek@gmail.com>
2024-02-15 19:31:53 +0100

    feat: adds new action implementations to handler map

--- src/services/sqs/mock-handler-map.ts
+++ src/services/sqs/mock-handler-map.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from 'express';
 import { createQueue } from './handlers/create-queue';
+import { deleteQueue } from './handlers/delete-queue';
+import { getQueueUrl } from './handlers/get-queue-url';
+import { listQueues } from './handlers/list-queues';
 
 export const mockHandlerMap = {
   CreateQueue: createQueue,
+  DeleteQueue: deleteQueue,
+  GetQueueUrl: getQueueUrl,
+  ListQueues: listQueues,
 } as Readonly<Record<string, (req: Request, res: Response) => Response>>;
