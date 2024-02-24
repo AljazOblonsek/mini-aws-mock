d8815a98f5a7e5ff4860a5f3502c38bfd820165c
AljazOblonsek <aljaz.oblonsek@gmail.com>
2024-02-24 13:41:52 +0100

    refactor: move create queue defaults and limits to constants

--- src/services/sqs/schemas/create-queue.schema.ts
+++ src/services/sqs/schemas/create-queue.schema.ts
@@ -1,18 +1,38 @@
 import { z } from 'zod';
 import { baseSchema } from '@/core';
+import {
+  DEFAULT_RECEIVE_MESSAGE_WAIT_TIME_IN_SECONDS,
+  DEFAULT_VISIBILITY_TIMEOUT_IN_SECONDS,
+  MAXIMUM_MESSAGE_SIZE_IN_KB,
+  MAXIMUM_RECEIVE_MESSAGE_WAIT_TIME_IN_SECONDS,
+  MAXIMUM_VISIBILITY_TIMEOUT_IN_SECONDS,
+  MINIMUM_MESSAGE_SIZE_IN_KB,
+} from '../constants/sqs-queue.constants';
 
 export const createQueueSchema = baseSchema.extend({
   QueueName: z.string(),
   Attributes: z
     .object({
-      VisibilityTimeout: z.coerce.number().min(0).max(43200).default(30),
-      ReceiveMessageWaitTimeSeconds: z.coerce.number().min(0).max(20).default(0),
-      MaximumMessageSize: z.coerce.number().min(1024).max(262144).default(262144),
+      VisibilityTimeout: z.coerce
+        .number()
+        .min(0)
+        .max(MAXIMUM_VISIBILITY_TIMEOUT_IN_SECONDS)
+        .default(DEFAULT_VISIBILITY_TIMEOUT_IN_SECONDS),
+      ReceiveMessageWaitTimeSeconds: z.coerce
+        .number()
+        .min(0)
+        .max(MAXIMUM_RECEIVE_MESSAGE_WAIT_TIME_IN_SECONDS)
+        .default(DEFAULT_RECEIVE_MESSAGE_WAIT_TIME_IN_SECONDS),
+      MaximumMessageSize: z.coerce
+        .number()
+        .min(MINIMUM_MESSAGE_SIZE_IN_KB)
+        .max(MAXIMUM_MESSAGE_SIZE_IN_KB)
+        .default(MAXIMUM_MESSAGE_SIZE_IN_KB),
     })
     .default({
-      VisibilityTimeout: 30,
-      ReceiveMessageWaitTimeSeconds: 0,
-      MaximumMessageSize: 262144,
+      VisibilityTimeout: DEFAULT_VISIBILITY_TIMEOUT_IN_SECONDS,
+      ReceiveMessageWaitTimeSeconds: DEFAULT_RECEIVE_MESSAGE_WAIT_TIME_IN_SECONDS,
+      MaximumMessageSize: MAXIMUM_MESSAGE_SIZE_IN_KB,
     }),
 });
 
