a7ce63442ca55bd3eb09ec46842e24af443a7b21
AljazOblonsek <aljaz.oblonsek@gmail.com>
2024-09-08 19:15:08 +0200

    feat: add regex validation for kms key alias

--- packages/shared/src/dtos/kms-key-create-request.dto.ts
+++ packages/shared/src/dtos/kms-key-create-request.dto.ts
@@ -1,14 +1,17 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class KmsKeyCreateRequestDto {
   @ApiProperty()
-  @IsString()
+  @Matches(/^[a-z-]+$/)
+  @MinLength(2)
+  @MaxLength(64)
   @IsNotEmpty()
   alias: string;
 
   @ApiPropertyOptional()
   @IsString()
   @IsOptional()
+  @MaxLength(256)
   description?: string;
 }
